2da2a4ff4f5cb8278c8f512d2ac7cd3559def7bf
aimdexter <draimade@gmail.com>
2022-03-11 22:20:27 +0100

    index mobile version done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./public/**/*.{html,js}",
+    "./public/*",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'goldit': '#FFFFFF',
+        'grayit': '#D0D6F9',
+        'blueit': '#0B0D17',
+      },
+      fontFamily: {
+        'Bellefair': ['Bellefair', 'sans-serif'],
+        'Barlow': ['Barlow', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }

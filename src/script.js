4a677217f5e23900cc5fc4befdab2005dfaaef41
aimdexter <draimade@gmail.com>
2022-03-28 23:30:15 +0000

    testin json

--- src/script.js
+++ src/script.js
@@ -1,12 +1,25 @@
-const title = 'hi';
-const author = 'aimade';
-const likes = '30';
-let result = `The blog called ${title} by ${author} has ${likes}`;
-console.log(result);
-let html = `
-<h2>${title}</h2>
-<p>${author}</p>
-<span>${likes}<:span>
-`
-console.log(html);
-console.log(html);
+function fetchData() {
+    fetch('moon.json')
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            let output = '<h2 class="mb-4">Countries</h2>'
+            data.forEach((item) => {
+                output += `
+        <ul class="list-group mb-3">
+        <img src="${item.images.png}" alt="">
+          <li class="list-group-item">${item.name}</li>
+          <li class="list-group-item">${item.description}</li>
+          <li class="list-group-item">${item.distance}</li>
+          <li class="list-group-item">${item.travel}</li>
+        </ul>
+      `
+            })
+            document.getElementById('country').innerHTML = output
+        })
+        .catch((error) => {
+            console.log(`Error Fetching data : ${error}`)
+            document.getElementById('country').innerHTML = 'Error Loading Data'
+        })
+}
+fetchData()

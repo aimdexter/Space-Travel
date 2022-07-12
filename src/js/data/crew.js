62c8584519e2cd431b2c229bae66db1b820a0c9b
aimdexter <draimade@gmail.com>
2022-07-12 19:52:38 +0100

    technology mobile done

--- src/js/data/crew.js
+++ src/js/data/crew.js
@@ -5,34 +5,33 @@ const bioo = document.querySelector('.bio');
 
 // fetching data from json
 const renderDetails = async (nameeu) => {
-    const res = await fetch(`http://localhost:3000/crew/` + `?name=${nameeu}`);
-    if (!res.ok) {
-        window.location.replace("/");
-    }
-    const reponse = await res.json();
-    const name = `
+  const res = await fetch(`http://localhost:3000/crew/` + `?name=${nameeu}`);
+  if (!res.ok) {
+    window.location.replace("/");
+  }
+  const reponse = await res.json();
+  const name = `
     ${reponse[0].name}
   `
-    const role = `
+  const role = `
     ${reponse[0].role}
   `
-    const bio = `
+  const bio = `
   ${reponse[0].bio}
 `
-    const image = reponse[0].images.png;
-    document.getElementById("crew-image").src = image;
+  const image = reponse[0].images.png;
+  document.getElementById("crew-image").src = image;
 
-    crewnamee.innerHTML = name;
-    rolee.innerHTML = role;
-    bioo.innerHTML = bio;
+  crewnamee.innerHTML = name;
+  rolee.innerHTML = role;
+  bioo.innerHTML = bio;
 }
 
 let buttonList = document.querySelectorAll("button");
 buttonList.forEach(function (i) {
-    i.addEventListener("click", function (e) {
-        console.log(i.value);
-        renderDetails(i.value);
-    })
+  i.addEventListener("click", function (e) {
+    renderDetails(i.value);
+  })
 })
 
 
---
+++ src/js/data/technology.js
@@ -0,0 +1,37 @@
+// javascript for destination.html
+const rolee = document.querySelector('.role');
+const crewnamee = document.querySelector('.crewname');
+const bioo = document.querySelector('.bio');
+
+// fetching data from json
+const renderDetails = async (nameeu) => {
+    const res = await fetch(`http://localhost:3000/technology/` + `?name=${nameeu}`);
+    if (!res.ok) {
+        window.location.replace("/");
+    }
+    const reponse = await res.json();
+    const name = `
+    ${reponse[0].name}
+  `
+    const role = `
+    ${reponse[0].role}
+  `
+    const descriptionn = `
+  ${reponse[0].description}
+`
+    const image = reponse[0].images.landscape;
+    document.getElementById("image").src = image;
+
+    crewnamee.innerHTML = name;
+    bioo.innerHTML = descriptionn;
+}
+
+let buttonList = document.querySelectorAll("button");
+buttonList.forEach(function (i) {
+    i.addEventListener("click", function (e) {
+        renderDetails(i.value);
+    })
+})
+
+
+// window.addEventListener('DOMContentLoaded', () => renderDetails());

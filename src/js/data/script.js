718c2045a65614296be77ee98ff9b1104857454a
aimdexter <draimade@gmail.com>
2022-07-12 16:39:52 +0100

    destination page done using javascript

--- src/js/data/script.js
+++ src/js/data/script.js
@@ -3,6 +3,7 @@ const destinationdesc = document.querySelector('.description');
 const destinationname = document.querySelector('.namedest');
 const dests = document.querySelector('.dests');
 const avg = document.querySelector('.avg');
+const day = document.querySelector('.days');
 
 // fetching data from json
 const renderDetails = async (nameeu) => {
@@ -10,6 +11,7 @@ const renderDetails = async (nameeu) => {
   if (!res.ok) {
     window.location.replace("/");
   }
+  console.log(res);
   const descr = await res.json();
   const desc = `
     ${descr[0].description}
@@ -20,17 +22,17 @@ const renderDetails = async (nameeu) => {
   const avgdata = `
   ${descr[0].distance}
 `
+  const period = `
+${descr[0].travel}
+`
   const image = descr[0].images.png;
   document.getElementById("image").src = image;
 
   destinationdesc.innerHTML = desc;
   destinationname.innerHTML = name;
   avg.innerHTML = avgdata;
-  // dests.innerHTML = names;
+  day.innerHTML = period;
 }
-
-
-
 
 let buttonList = document.querySelectorAll("button");
 buttonList.forEach(function (i) {

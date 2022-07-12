eedb65b1209b3d7131ae8155efaa604e9962a7de
aimdexter <draimade@gmail.com>
2022-07-12 21:46:16 +0100

    optimazed pages in javascript

---
+++ src/js/data/menu.js
@@ -0,0 +1,9 @@
+const home = "/index.html";
+const destinationpage = "/public/destination.html";
+const crewpage = "/public/crew.html";
+const technologypage = "/public/Technology.html";
+
+document.getElementById("home").href = home;
+document.getElementById("destination").href = destinationpage;
+document.getElementById("crew").href = crewpage;
+document.getElementById("technology").href = technologypage;
